d7e93de6a763a047ef2cee1f2519f8bff1ff370b
sujush <sujush@naver.com>
2024-11-24 18:33:46 +0900

    통관상태 로그인 해제

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -14,16 +14,17 @@ const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const router = useRouter();
   const { isAuthenticated, checkAuth } = useAuthStore();
 
+  // 여기부터 코드 수정
   useEffect(() => {
     console.log("ProtectedRoute 시작, isAuthenticated:", isAuthenticated);
-    checkAuth();  // 페이지 접근 시 쿠키에서 토큰 확인
+    checkAuth(); // 페이지 접근 시 쿠키에서 토큰 확인
 
     if (!isAuthenticated) {
-      console.log("isAuthenticated가 false여서 로그인 페이지로 이동");
-      router.push('/login');
+      console.log("isAuthenticated가 false여도 리다이렉트는 비활성화되었습니다.");
+      // router.push('/login'); // 리다이렉션 제거
     }
   }, [isAuthenticated, router, checkAuth]);
-
+  // 여기까지 코드 수정완료
   if (!isAuthenticated) {
     console.log("ProtectedRoute에서 isAuthenticated false로 반환");
     return null;
